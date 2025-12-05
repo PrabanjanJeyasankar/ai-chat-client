023d20f7d47ac858ca3b5abe2018fc713ae7f6a3
Prabanjan Jeyasankar <prabanjanjeyasankar@gmail.com>
2025-12-05 22:10:16 +0530

    feat - Initial Commit to save the Folder Structure.

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  return (
+    <>
+      <div>AI Chat</div>
+      <div>
+        <Button className='bg-primary'>Am i Blue?</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
